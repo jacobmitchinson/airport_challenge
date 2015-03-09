4b29aad354663153c99ddca7f088d8aad9c90a94
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-09 14:53:27 +0000

    Update weather_module_spec.rb
    Removed redundant space.

--- spec/weather_module_spec.rb
+++ spec/weather_module_spec.rb
@@ -6,7 +6,6 @@ describe Weather do
 
   let(:weather_test) { WeatherModule.new}
 
-
   it 'should return stormy' do
     allow(weather_test).to receive(:random_number).and_return(1)
     expect(weather_test.weather).to eq 'stormy'
